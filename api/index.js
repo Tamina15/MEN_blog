6405576965be5877a6d72e94bcdf46f4e083c72b
Tamina15 <minhnhat66442658@gmail.com>
2023-08-31 17:35:20 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const app = express();
 // variable
 const port = process.env.PORT || 15000;
 const express_layout = require('express-ejs-layouts');
-const connectDB = require('./config/db');
+const connectDB = require('../config/db');
 
 // connect to DB
 connectDB();
@@ -19,20 +19,22 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express_layout);
 
-app.use('/', require('./routes/index.route'));
-app.use('/user', require('./routes/user.route'));
-app.use('/blog', require('./routes/blog.route'));
-
+app.use('/', require('../routes/index.route'));
+app.use('/user', require('../routes/user.route'));
+app.use('/blog', require('../routes/blog.route'));
+app.get('/test', function (req, res) {
+  res.send({ status: 500 }, "testing");
+})
 // routing
 
 
 // 404
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  })
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+})
 
 // listen
 app.listen(port, function () {
-    console.log("App running at " + port);
+  console.log("App running at " + port);
 })
