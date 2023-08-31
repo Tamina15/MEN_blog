7474c35b569378a6cc9132596c0577aaa83e9a02
Tamina15 <minhnhat66442658@gmail.com>
2023-08-31 17:40:56 +0700

    Update blog.route.js

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -3,12 +3,12 @@ const express = require('express');
 // const app = express();
 const router = express.Router();
 const { RedirectIndex } = require('../controllers/index.controller');
-const {GetBlogByID} = require('../controllers/blog.controller');
+const { GetBlogByID } = require('../controllers/blog.controller');
 // routing
-router.get('/',RedirectIndex);
+router.get('/', RedirectIndex);
 router.get('/:id', GetBlogByID);
 router.get('/test', function (req, res) {
-    res.send('Testing');
+    res.send('Testing Blog');
 })
 
 module.exports = router;
