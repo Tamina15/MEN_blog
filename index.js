5b1d73917af354aec3262116bebe900c3e45888f
Tamina15 <minhnhat66442658@gmail.com>
2023-08-31 17:28:22 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+// variable
+const port = process.env.PORT || 15000;
+const express_layout = require('express-ejs-layouts');
+const connectDB = require('./config/db');
+
+// connect to DB
+connectDB();
+
+// setting
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+// middleware
+app.use(express.static('public'));
+app.use(express_layout);
+
+app.use('/', require('./routes/index.route'));
+app.use('/user', require('./routes/user.route'));
+app.use('/blog', require('./routes/blog.route'));
+
+// routing
+
+
+// 404
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  })
+
+// listen
+app.listen(port, function () {
+    console.log("App running at " + port);
+})
