b0535b1fdedcf28da4ccfe08e3c1a209092a7dbc
Tamina15 <minhnhat66442658@gmail.com>
2023-09-07 21:14:29 +0700

    Add Get Newest Blogs

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,6 +1,9 @@
 const Blog = require('../model/Blog');
+
 async function GetBlogByID(req, res) {
     const blog = await Blog.findById(req.params.id);
-    res.render('blog', { blog });
+    const newest_blog = await Blog.find().sort({ x: -1 }).limit(3);
+    res.render('blog', { blog, newest_blog });
 }
+
 module.exports = { GetBlogByID };
