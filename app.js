0614695b1a86275208064f46719a5a60d9261c97
Tamina15 <minhnhat66442658@gmail.com>
2023-09-07 21:09:30 +0700

    Add session

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const app = express();
 const port = process.env.PORT || 15000;
 const express_layout = require('express-ejs-layouts');
 const connectDB = require('./config/db');
-
+const bodyParser = require('body-parser');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 // connect to DB
 connectDB();
 
@@ -19,6 +21,16 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express_layout);
 
+
+app.use(session({
+  secret: process.env.SESSION_SECRET_KEY,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use('/', require('./routes/index.route'));
 app.use('/user', require('./routes/user.route'));
 app.use('/blog', require('./routes/blog.route'));
@@ -28,11 +40,11 @@ app.use('/blog', require('./routes/blog.route'));
 
 // 404
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  })
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+})
 
 // listen
 app.listen(port, function () {
-    console.log("App running at " + port);
+  console.log("App running at " + port);
 })
