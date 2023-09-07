051d0e568511366ae2b7f70e65ff8a80227c1188
Tamina15 <minhnhat66442658@gmail.com>
2023-09-07 21:14:49 +0700

    Add Search, SetSession controllers

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,19 +1,51 @@
 const Blog = require('../model/Blog');
+const User = require('../model/User');
 const moment = require('moment');
-const db = require('mongoose');
 
 async function GetIndexPage(req, res) {
     try {
-        // const updat = await Blog.updateMany({ createdAt: "2023-08-31T02:03:55.552+00:00" }, { $set: { preview: "null" } });
+
+        const title = 'My Blog';
+        // const search = await User.find({_id:"64f0086923f43238c6b817ca"});
+        // const random = await User.aggregate([{ $sample: { size: 5 } }]).exec(); // You want to get 5 docs
+        // console.log(random);
         const data = await Blog.find();
-        // console.log(updat);
-        res.render('index', { moment, data });
+        const datas = [title, moment, data];
+        res.render('index', { title, moment, data });
     } catch (error) {
         console.log(error);
     }
+}
+async function Search(req, res) {
+    const title = "Search";
+    try {
+        const search = req.body.search;
+        const searchNoSpecialChar = search.replace(/[^a-zA-Z0-9 ]/g, "");
+        const data = await Blog.find({
+            $or: [
+                {
+                    title: { $regex: new RegExp(searchNoSpecialChar, 'i') }
+                }
+            ]
+        });
+        res.render('index', { title, moment, data, search });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+    // console.log(req.body);
 }
 function RedirectIndex(req, res) {
     res.redirect('/');
 }
 
-module.exports = { GetIndexPage, RedirectIndex };
+function SetSession(req, res, next) {
+    if (req.session.user != undefined) {
+        console.log(req.session);
+        res.locals.user = req.session.user;
+        res.locals.id = req.session.id;
+    }
+    next();
+}
+
+module.exports = { GetIndexPage, Search, RedirectIndex, SetSession };
