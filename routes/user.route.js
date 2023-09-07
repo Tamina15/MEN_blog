4fa6dfefed232b0cbf89d5a85cff265df49322a2
Tamina15 <minhnhat66442658@gmail.com>
2023-09-07 21:13:27 +0700

    Add POST route, CheckLogIn middleware

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,17 +2,25 @@ require('dotenv').config();
 const express = require('express');
 // const app = express();
 const router = express.Router();
-const {UserPage, Login, Register } = require('../controllers/user.controller');
+const { UserPage, GetLoginPage, GetRegisterPage, Login, Logout, Register, CheckLoggedIn, CheckNotLoggedIn} = require('../controllers/user.controller');
 
 // routing
-router.get('/',UserPage);
-
-router.get('/login', Login);
-
-router.get('/register', Register);
-
 router.get('/test', function (req, res) {
     res.send('Testing User');
 })
+router.get('/login', CheckNotLoggedIn, GetLoginPage);
+
+router.get('/register', CheckNotLoggedIn,GetRegisterPage);
+
+router.post('/login',CheckNotLoggedIn, Login);
+
+router.post('/register',CheckNotLoggedIn, Register);
+
+router.use(CheckLoggedIn);
+
+router.get('/', UserPage);
+
+router.get('/logout', Logout)
+
 
 module.exports = router;
